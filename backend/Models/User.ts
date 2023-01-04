d0914a3a0810791ccb888ad7ed67abf62f686887
Aline Gonçalves Fontes Chagas <alinegfchagas@hotmail.com>
2023-01-04 11:45:21 -0300

    criação de modelos para input e output de dados

---
+++ backend/Models/User.ts
@@ -0,0 +1,11 @@
+export interface User {
+    id:string,
+name: string,
+adress: string,
+phone: number,
+email: string,
+birth_date:Date,
+login: string,
+password: string,
+profile_pic: string 
+}
