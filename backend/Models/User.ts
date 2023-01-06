7e7c5a844bada229591b443d5418d3571e855382
Aline Gonçalves Fontes Chagas <alinegfchagas@hotmail.com>
2023-01-05 22:39:15 -0300

    encapsulamento de modelo de user, getters e setters, primeiro endpoint para signup

--- backend/Models/User.ts
+++ backend/Models/User.ts
@@ -1,4 +1,4 @@
-export interface User {
+export interface IUserDB {
   id: string;
   name: string;
   adress: string;
@@ -10,3 +10,71 @@ export interface User {
   profile_pic: string;
 }
 
+export class UserDB {
+  constructor(
+    private id: string,
+    private name: string,
+    private adress: string,
+    private phone: number,
+    private email: string,
+    private birth_date: Date,
+    private login: string,
+    private password: string,
+    private profile_pic: string
+  ) {}
+
+  public getId = () => {
+    return this.id;
+  };
+  public setId = (newId: string) => {
+    this.id = newId;
+  };
+  public getName = () => {
+    return this.name;
+  };
+  public setName = (newName: string) => {
+    this.name = newName;
+  };
+  public getAdress = () => {
+    return this.adress;
+  };
+  public setAdress = (newAdress: string) => {
+    this.adress = newAdress;
+  };
+  public getPhone = () => {
+    return this.phone;
+  };
+  public setPhone = (newPhone: number) => {
+    this.phone = newPhone;
+  };
+  public getEmail = () => {
+    return this.email;
+  };
+  public setEmail = (newEmail: string) => {
+    this.email = newEmail;
+  };
+  public getBirthDate = () => {
+    return this.birth_date;
+  };
+  public setBirthDate = (newBirthDate: Date) => {
+    this.birth_date = newBirthDate;
+  };
+  public getLogin = () => {
+    return this.login;
+  };
+  public setLogin = (newLogin: string) => {
+    this.login = newLogin;
+  };
+  public getPassword = () => {
+    return this.password;
+  };
+  public setPassword = (newPassword: string) => {
+    this.password = newPassword;
+  };
+  public getProfilePic = () => {
+    return this.profile_pic;
+  };
+  public setProfilePic = (newProfilePic: string) => {
+    this.profile_pic = newProfilePic;
+  };
+}
--- backend/database/UserDatabase.ts
+++ backend/database/UserDatabase.ts
@@ -1,11 +1,42 @@
+import { IUserDB, UserDB } from "../Models/User";
 import { BaseDatabase } from "./BaseDatabase";
 
-export class UserDatabase extends BaseDatabase{
-    public static CLIENTS = "Clients";
-    
-    const UserDB ={
-        
-    }
+export class UserDatabase extends BaseDatabase {
+  public static CLIENTS = "Clients";
 
+  public UserDBModel = (user: UserDB): IUserDB => {
+    const UserDB: IUserDB = {
+      id: user.getId(),
+      name: user.getName(),
+      adress: user.getAdress(),
+      phone: user.getPhone(),
+      email: user.getEmail(),
+      birth_date: user.getBirthDate(),
+      login: user.getLogin(),
+      password: user.getPassword(),
+      profile_pic: user.getProfilePic(),
+    };
+    return UserDB;
+  };
+  
+  public signUp = async (user: UserDB) => {
+    await BaseDatabase.connection(UserDatabase.CLIENTS).insert({
+      id: user.getId(),
+      name: user.getName(),
+      adress: user.getAdress(),
+      phone: user.getPhone(),
+      email: user.getEmail(),
+      birth_date: user.getBirthDate(),
+      login: user.getLogin(),
+      password: user.getPassword(),
+      profile_pic: user.getProfilePic(),
+    });
+  };
 
+//   public login = async (user: UserDB) => {
+//     await BaseDatabase.connection(UserDatabase.CLIENTS).where({
+//       login: user.getLogin(),
+//       password: user.getPassword()
+//     });
+//   };
 }
